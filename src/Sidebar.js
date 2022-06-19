e1f8e414053b7e16935627862157574dd06053f1
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2022-06-19 17:54:08 +0530

    sidebar header added

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import './Sidebar.css'
+import {Avatar, IconButton} from '@mui/material'
+import ChatIcon from '@mui/icons-material/Chat';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
-        Sidebar
+        Sidebar 
+        <div className='sidebar_header'>
+        <Avatar />
+            <div className='sidebar_headerRight'>
+              
+              <IconButton>
+                <DonutLargeIcon />
+              </IconButton>
+
+              <IconButton>
+                <ChatIcon/>
+              </IconButton>
+
+              <IconButton>
+                <MoreVertIcon />
+              </IconButton>
+           
+           
+            </div>
+        </div>
        
     </div>
 
