33bb582fd7a9bac81f6ca487766f0b93568c6fc5
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2022-06-19 19:34:06 +0530

    chat header added

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import './Chat.css'
+import {Avatar, IconButton} from '@mui/material'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import AttachFileIcon from '@mui/icons-material/AttachFile';
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
-  )
-}
+    <div className='chat'>
+        <div className="chat__header">
+            <Avatar />
+
+            <div className="chat__headerInfo">
+                <h3>Room Name</h3>
+                <p> Last Seen at ... </p>
+            </div>
+
+            <div className="chat__headerRight">
+              <IconButton>
+                <SearchOutlinedIcon />
+              </IconButton>
+              
+              <IconButton>
+              <AttachFileIcon />
+              </IconButton>
+
+              <IconButton>
+                <MoreVertIcon /> 
+              </IconButton>
+          
+
+            </div>
+        </div>
+    </div>
+)}
 
 export default Chat
