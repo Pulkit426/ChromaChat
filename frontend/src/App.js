142e661f646fd974617a967ea8edc11ef5b02a35
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2022-06-21 23:02:22 +0530

    axios get functionality added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
 import Pusher from 'pusher-js'
+import messageService from './services/messages'
 
 function App() {
+  const [messages, setMessages] = useState([])
+  
+  const initializeAllMessages = async () => {
+    const allMessages = await messageService.getAll()
+    setMessages(allMessages)
+  }
+
+  useEffect(() => {
+    initializeAllMessages()
+  }, [])
+
   useEffect(() => {
     const pusher = new Pusher('a71c2a5658b701ee38fc', {
       cluster: 'ap2'
     });
 
     const channel = pusher.subscribe('messages');
-    channel.bind('inserted', function(data) {
-      alert(JSON.stringify(data));
+    channel.bind('inserted', function(newMessage) {
+      alert(JSON.stringify(newMessage));
+      setMessages([...messages,newMessage])
     });
 
-  }, [])
+  }, [messages])
+
+  console.log(messages)
 
   return (
     <div className="app">
