8a9c119d4360c6870db1ea876213a4b868b7316d
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2022-06-21 15:19:29 +0530

    added pusher-js subscription

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import { useEffect } from 'react';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import Pusher from 'pusher-js'
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher('a71c2a5658b701ee38fc', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      alert(JSON.stringify(data));
+    });
+
+  }, [])
+
   return (
     <div className="app">
       <div className='app_body'>
