509b9de8b89ce23706b4561624f44dbda675e84e
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2022-06-27 23:48:38 +0530

    GET, POST added for chat room

---
+++ backend/controllers/rooms.js
@@ -0,0 +1,31 @@
+const router = require('express').Router()
+const Room = require('../models/room')
+
+router.get('/', (req,res) => {
+    Room.find(
+        (err,data) => {
+        if(err){
+            res.status(500).send(err)
+        }
+        else{
+            res.status(200).send(data)
+        }
+    })
+
+})
+
+
+router.post('/', (req,res) => {
+    const newRoom = req.body
+
+    Message.create(newRoom, (err,data) => {
+        if(err){
+            res.status(500).send(err)
+        }
+        else{
+            res.status(201).send(data)
+        }
+    })
+})
+
+module.exports = router
