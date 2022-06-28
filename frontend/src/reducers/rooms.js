705a1b977a251780771f97a7d6935cf4e675d1f4
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2022-06-28 19:49:44 +0530

    room reducer added

---
+++ frontend/src/reducers/rooms.js
@@ -0,0 +1,25 @@
+const roomsReducer = (state=[],action) => {
+    switch(action.type){
+        case 'INITIAL_ROOMS':
+            return [...action.data]
+        case 'NEW_ROOM': 
+            return [...state,action.data]
+    }
+}
+
+
+export const intitializeRooms = (rooms) => {
+    return {
+        type: 'INITIAL_ROOMS',
+        data: rooms
+    }
+}
+
+export const newRoom = (room) => {
+    return{
+        type: 'NEW_ROOM',
+        data: room
+    }
+}
+
+export default roomsReducer
