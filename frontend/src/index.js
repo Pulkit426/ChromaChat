604258b8720dae4e94492196009c88597e179912
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2022-06-28 20:14:26 +0530

    new store created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux';
 import messagesReducer from './reducers/messages';
+import roomsReducer from './reducers/rooms';
 
-const store = createStore(messagesReducer)
+const reducer = combineReducers({
+  messages: messagesReducer,
+  rooms: roomsReducer
+})
 
+const store = createStore(reducer) 
+console.log("STORE", store.getState())
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
