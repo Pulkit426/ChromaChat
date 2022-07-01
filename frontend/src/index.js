a6ea7de054c6222c5a9098bdb3db900ee7323d72
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2022-07-01 23:34:00 +0530

    redux thunk added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import thunk from "redux-thunk" 
+import { applyMiddleware } from 'redux';
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux';
 import messagesReducer from './reducers/messages';
@@ -12,7 +14,7 @@ const reducer = combineReducers({
   rooms: roomsReducer
 })
 
-const store = createStore(reducer) 
+const store = createStore(reducer, applyMiddleware(thunk)) 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
